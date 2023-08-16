b9422f530a8c3e0e84c816049805640f6cf6ef1e
V-kwoink <ktoanen@vaisonet.com>
2023-08-16 10:56:14 +0200

    récriture su script
    - ajout d'un exit code

--- Artifacts/windows-install-econnecteur/artifact.ps1
+++ Artifacts/windows-install-econnecteur/artifact.ps1
@@ -1,7 +1,16 @@
 Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
-Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS" -Wait
 
+$exePath = "C:\ECO\update-econnecteur.exe"
+$arguments = "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS"
 
+Start-Process -FilePath $exePath -ArgumentList $arguments -Wait
 
+$exitCode = $LASTEXITCODE
 
-
+if ($exitCode -eq 0) {
+    Write-Host "La commande s'est exécutée avec succès."
+    exit 0
+} else {
+    Write-Host "Une erreur s'est produite lors de l'exécution de la commande."
+    exit 1
+}
