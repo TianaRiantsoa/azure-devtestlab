7644a5935cee6221695292767fd7dbb9983f9c1c
V-kwoink <ktoanen@vaisonet.com>
2023-08-21 16:40:55 +0200

    Backup artifact.ps1

--- Artifacts/windows-install-econnecteur/artifact.ps1
+++ Artifacts/windows-install-econnecteur/artifact.ps1
@@ -1,9 +1,3 @@
-pushd C:\ECO\
+Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
 
-Invoke-WebRequest -UseBasicParsing -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
-
-Start-Process update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS" -Wait
-
-popd
-
-exit 0
+Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS" -Wait	
