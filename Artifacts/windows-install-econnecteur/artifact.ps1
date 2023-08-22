edbac3149ee2cf4c5e47e77ef71ebe676aa1c1c9
V-kwoink <ktoanen@vaisonet.com>
2023-08-22 11:23:41 +0200

    correction install EC : Objet metier Sage rendait la durée infinie

--- Artifacts/windows-install-econnecteur/artifact.ps1
+++ Artifacts/windows-install-econnecteur/artifact.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
 
-Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS" -Wait	
+Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS"
