3dbdf6cd95ad550bf161761769f6732a28e4f5b9
V-kwoink <ktoanen@vaisonet.com>
2023-08-22 10:38:32 +0200

    ajout artefact de test pour install EC

---
+++ Artifacts/windows-install-econnecteur - test/artifact.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
+
+Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS"
