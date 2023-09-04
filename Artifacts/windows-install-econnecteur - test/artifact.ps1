95abfee529c9cbab0a06036f5eb3126d2695ec6b
V-kwoink <ktoanen@vaisonet.com>
2023-09-04 17:06:55 +0200

    upadte url install Econnecteur test

--- Artifacts/windows-install-econnecteur - test/artifact.ps1
+++ Artifacts/windows-install-econnecteur - test/artifact.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/update-econnecteur.exe -OutFile C:\ECO\update-econnecteur.exe
+Invoke-WebRequest -Uri https://builds.vaisonet.com/liveupdate_server/temp_test_v8.exe -OutFile C:\ECO\update-econnecteur.exe
 
 Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL", "/NORESTART", "/FORCECLOSEAPPLICATIONS"
