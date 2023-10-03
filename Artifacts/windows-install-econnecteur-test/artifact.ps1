014d0d7ddd228e25677c29f422f49a508ed2a14c
V-kwoink <ktoanen@vaisonet.com>
2023-10-03 14:51:19 +0200

    correction chemin shortcut sur les artefacts

--- Artifacts/windows-install-econnecteur-test/artifact.ps1
+++ Artifacts/windows-install-econnecteur-test/artifact.ps1
@@ -7,14 +7,14 @@ Start-Process C:\ECO\update-econnecteur.exe -ArgumentList "/SILENT", "/NOCANCEL"
 #on va chercher le script d'update et on va créer le raccourci sur le bureau
 
 $PATH = "C:\ECO\update-EC.ps1"
-
+$PATHSH = Get-ChildItem C:\Users\ -Filter ECT* | Select-Object -ExpandProperty Name
 wget https://raw.githubusercontent.com/Vaisonet/azure-devtestlab/master/samples/DevTestLabs/Scripts/UPEC/UPEC.ps1 -OutFile $PATH
 
 $CMD = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $CMDARG = "-ep bypass -file $PATH"
 
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Update E-Connecteur.lnk")
+$Shortcut = $WshShell.CreateShortcut("C:\Users\$PATHSH\Desktop\Update E-Connecteur.lnk")
 $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $Shortcut.Arguments = "$CMDARG"
 $Shortcut.Save()
